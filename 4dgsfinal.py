f2aa849fd7bce7884654fec6a2e481dcee229211
Utkarsh Pawade <utkarshpawadecse@gmail.com>
2025-06-26 22:54:20 +0530

    Update 4dgsfinal.py

--- 4dgsfinal.py
+++ 4dgsfinal.py
@@ -59,14 +59,7 @@ from time import time
 import concurrent.futures
 import mmcv
 from utils.params_utils import merge_hparams
-import copy
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-to8b = lambda x : (255*np.clip(x.cpu().numpy(),0,1)).astype(np.uint8)
-
-try:
-    from torch.utils.tensorboard import SummaryWriter
-    TENSORBOARD_FOUND = True
 except ImportError:
     TENSORBOARD_FOUND = False
 
